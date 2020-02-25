2dbc38fede0f2f5b0d14224bf1828ebb7102092e
cstuser <cstuser@d23908-jzrtdw2>
2020-02-25 11:03:04 -0500

    update

--- src/sandbox/Sandbox.java
+++ src/sandbox/Sandbox.java
@@ -15,7 +15,21 @@ public class Sandbox {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        System.out.println("Adding an output to my app!");
+        
+        Integer a = new Integer(0);
+        int b = 2;
+        boolean result = (a==b);
+        
+        System.out.println("Is a = b? " + result);
+        System.out.println("The value of a is " + a);
+        System.out.println("The value of b is " + b);
+        
+        System.out.println("Is a =b?" + (a=b));
+        System.out.println("The value of a is " +a);
+        System.out.println("The value of b is " +b);
+        
+        
     }
     
 }
